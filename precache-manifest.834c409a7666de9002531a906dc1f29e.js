68bb8a19349b55050b0fbea461e3fd089c2688b3
archer <econarcher2010@gmail.com>
2023-03-06 15:26:03 +0800

    Updates

---
+++ precache-manifest.834c409a7666de9002531a906dc1f29e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11492334271461ef11930e6bde92dc80",
+    "url": "/Practice_react_forms/index.html"
+  },
+  {
+    "revision": "71df79090107cca5aa54",
+    "url": "/Practice_react_forms/static/js/2.28a53448.chunk.js"
+  },
+  {
+    "revision": "0569f44ea0e392e757d0",
+    "url": "/Practice_react_forms/static/js/main.1ae19e75.chunk.js"
+  },
+  {
+    "revision": "e5451acf08ebb0da4ca5",
+    "url": "/Practice_react_forms/static/js/runtime-main.9aaf4199.js"
+  }
+]);
